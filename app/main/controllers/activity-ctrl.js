d8a67a0693fcbd921081d85e239e16cf3af1a221
Armand Grillet <armand.a.grillet@campus.tu-berlin.de>
2016-01-16 09:46:38 +0100

    Add activity's name parameter for the activity controller

--- app/main/controllers/activity-ctrl.js
+++ app/main/controllers/activity-ctrl.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('main')
-.controller('ActivityCtrl', function ($log) {
+  .controller('ActivityCtrl', function($log, $stateParams) {
 
-  $log.log('Hello from your Controller: ActivityCtrl in module main:. This is your controller:', this);
-
-});
+    $log.log('Hello from your Controller: ActivityCtrl in module main:. This is your controller:', this);
+    $log.log($stateParams.name);
+  });
--- app/main/main.js
+++ app/main/main.js
@@ -5,31 +5,34 @@ angular.module('main', [
   'ui.router',
   // TODO: load other modules selected during generation
 ])
-.config(function ($stateProvider, $urlRouterProvider) {
+  .config(function($stateProvider, $urlRouterProvider) {
 
-  // ROUTING with ui.router
-  $urlRouterProvider.otherwise('/plan');
-  $stateProvider
-    // this state is placed in the <ion-nav-view> in the index.html
-    .state('plan', {
-      url: '/plan',
-      templateUrl: 'main/templates/plan.html',
-      controller: 'PlanCtrl as ctrl'
-    })
-    .state('activity', {
-      url: '/activity',
-      templateUrl: 'main/templates/activity.html',
-      controller: 'ActivityCtrl as ctrl'
-    })
-    .state('property', {
-      url: '/property',
-      templateUrl: 'main/templates/property.html',
-      controller: 'PropertyCtrl as ctrl'
-    })
-    .state('preferences', {
-      url: '/preferences',
-      templateUrl: 'main/templates/preferences.html',
-      controller: 'PreferencesCtrl as ctrl'
-    });
+    // ROUTING with ui.router
+    $urlRouterProvider.otherwise('/plan');
+    $stateProvider
+      // this state is placed in the <ion-nav-view> in the index.html
+      .state('plan', {
+        url: '/plan',
+        templateUrl: 'main/templates/plan.html',
+        controller: 'PlanCtrl as ctrl'
+      })
+      .state('activity', {
+        url: '/activity',
+        templateUrl: 'main/templates/activity.html',
+        controller: 'ActivityCtrl as ctrl',
+        params: {
+          name: null
+        }
+      })
+      .state('property', {
+        url: '/property',
+        templateUrl: 'main/templates/property.html',
+        controller: 'PropertyCtrl as ctrl'
+      })
+      .state('preferences', {
+        url: '/preferences',
+        templateUrl: 'main/templates/preferences.html',
+        controller: 'PreferencesCtrl as ctrl'
+      });
 
-});
+  });
