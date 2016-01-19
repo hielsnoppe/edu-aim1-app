bfc71f60e88868a15c83b7dc2096894c8ace981b
Armand Grillet <armand.a.grillet@campus.tu-berlin.de>
2016-01-19 14:11:53 +0100

    Beginning of activty

--- app/main/controllers/activity-ctrl.js
+++ app/main/controllers/activity-ctrl.js
@@ -1,7 +1,43 @@
 'use strict';
 angular.module('main')
-  .controller('ActivityCtrl', function($log, $stateParams) {
+  .controller('ActivityCtrl', function($ionicModal, $scope, $ionicPopup, $log, $stateParams) {
+    this.name = $stateParams.name;
 
-    $log.log('Hello from your Controller: ActivityCtrl in module main:. This is your controller:', this);
-    $log.log($stateParams.name);
+    this.filters = [];
+    this.availableFilters = [];
+    if (this.name === 'Cinema') {
+      this.availableFilters = [{
+        name: 'Movie'
+      }];
+    }
+
+    $ionicModal.fromTemplateUrl('main/templates/filterModal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.filterModal = modal;
+    });
+
+    this.openFilterModal = function() {
+      if (this.availableFilters.length === 0) {
+        $ionicPopup.alert({
+          title: 'No more filters to add',
+          content: 'You already used all the filters.'
+        });
+      } else {
+        $scope.filterModal.show();
+      }
+    };
+
+    this.createFilter = function(filter, index) {
+      this.filters.push({
+        name: filter.name
+      });
+      this.availableFilters.splice(index, 1)
+      $scope.filterModal.hide();
+    };
+
+    this.closeModal = function() {
+      $scope.filterModal.hide();
+    };
   });
