711ced808c1bf5f78d4c7f16b6a23a04a1bbb2e5
Erik Berdonces Bonelo <sturgelose@gmail.com>
2016-01-19 15:18:27 +0100

    Create empty services and tests for Activities, Filters, Components and root service

---
+++ app/main/services/activity-serv.js
@@ -0,0 +1,6 @@
+'use strict';
+angular.module('main')
+.service('Activity', function ($log) {
+
+
+});
---
+++ app/main/services/component-serv.js
@@ -0,0 +1,7 @@
+'use strict';
+angular.module('main')
+.service('Component', function ($log) {
+
+  $log.log('Hello from your Service: Component in module main');
+
+});
