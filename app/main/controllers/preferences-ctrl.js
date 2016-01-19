26f202108578933066cad4079b6d41244949efc4
Erik Berdonces Bonelo <sturgelose@gmail.com>
2016-01-19 16:00:37 +0100

    Rename files to proper names. Create result controller-view

--- app/main/controllers/preferences-ctrl.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module('main')
-.controller('PreferencesCtrl', function ($log) {
-
-  $log.log('Hello from your Controller: PreferencesCtrl in module main:. This is your controller:', this);
-
-});
---
+++ app/main/controllers/result-ctrl.js
@@ -0,0 +1,7 @@
+'use strict';
+angular.module('main')
+.controller('ResultCtrl', function ($log) {
+
+  $log.log('Hello from your Controller: ResultCtrl in module main:. This is your controller:', this);
+
+});
