c62f1d8b5abe987bec725da6d3c08d57e97c511b
Jan <jan.kalkan@mailbox.org>
2016-02-14 14:58:26 +0100

    Result Detail Template Finished

--- app/main/controllers/result-detail-ctrl.js
+++ app/main/controllers/result-detail-ctrl.js
@@ -1,6 +1,8 @@
 'use strict';
 angular.module('main')
-.controller('ResultDetailCtrl', function ($log) {
+.controller('ResultDetailCtrl', function ($log,$stateParams, Root) {
+
+  this.activities = $stateParams.activities;
 
   $log.log('Hello from your Controller: ResultDetailCtrl in module main:. This is your controller:', this);
 
